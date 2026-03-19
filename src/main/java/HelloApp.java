f43d075fffaad8cfe775d986f907770bc62d5ff8
Pranav2511-eng <pv5291@srmist.edu.in>
2026-03-19 20:34:38 +0530

    uc4 implementation done

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -7,8 +7,6 @@ public class HelloApp {
 
             for (int i = 0; i < args.length; i++) {
                 System.out.print(args[i]);
-
-                // Add comma if not last name
                 if (i < args.length - 1) {
                     System.out.print(", ");
                 }
